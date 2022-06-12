a455aee16194eac5f7161747261325ae26ad7343
Saumya pandey <saumyapandey@Saumyas-iMac.local>
2022-06-12 11:44:36 +0530

    Updating with file

---
+++ app.py
@@ -0,0 +1,13 @@
+from crypt import methods
+import imp
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    return "Starting Machine learning Project"
+
+
+if __name__=="__main__":
+    app.run(debug=True)

4492dd417dcdf1c94b6a22ad6b718a1b40b62d6d
Saumya pandey <saumyapandey@Saumyas-iMac.local>
2022-06-16 17:48:50 +0530

    Update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app= Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine learning Project"
+    return "Starting Machine learning Project-next try"
 
 
 if __name__=="__main__":
